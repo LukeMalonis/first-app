cafb82ced60e2eb16bc0161e06f5f5c8ae2285f3
Luke_Malonis <malonisluke@gmail.com>
2025-09-29 19:18:20 -0400

    Chore: inital files

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"mesage": "Skibidi Toilet fortnite balls"}
+
+@app.post("/")
+async def post():
+    return {"message": "hello from the post route"}
+
+@app.put("/")
+async def put():
+    return {"message": "hello from the put route"}
